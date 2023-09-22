e6f14fde7e87af4984c9e3ebfecb4791c17bf78e
Parth Agrawal <74432215+AnakinDV1738@users.noreply.github.com>
2023-09-22 16:11:41 -0400

    Add files via upload
    adding code

---
+++ IntenseProgram.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# print(ord("H"))
+# print(ord("e"))
+# print(ord("l"))
+# print(ord("o"))
+# print(ord("W"))
+# print(ord("r"))
+# print(ord("d"))
+# print(ord(" "))
+
+insane_array = [chr(72),chr(101),chr(108),chr(108),chr(111),chr(32),chr(87),chr(111),chr(114),chr(108),chr(100)]
+
+for WHOKNOWS in insane_array:
+    print(WHOKNOWS, end = "")
